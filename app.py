15a5369103033fa051e0d0fee7cfb796415c7bdc
davidCrejenovschi <crejenoriginal.david@gmail.com>
2026-03-09 20:44:23 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_user_data():
+    # Cheie de acces hardcodata pentru GitHub
+    github_token = "ghp_AbCdEfGhIjKlMnOpQrStUvWxYz1234567890"
+    
+    headers = {
+        "Authorization": f"Bearer {github_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    
+    response = requests.get("https://api.github.com/user", headers=headers)
+    return response.json()
